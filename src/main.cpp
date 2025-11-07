8f9d8a1125025bfdb67a23e697a96b963b61fc41
Janis Edvarts Lacis <janislacis06@gmail.com>
2025-11-07 12:53:18 +0200

    create Message and GlobalData struct

--- src/main.cpp
+++ src/main.cpp
@@ -1,23 +1,44 @@
 #include <ifaddrs.h>
 #include <netdb.h>
-#include <stdio.h>
+#include <unordered_map>
+#include "net/if.h"
+
+struct Message {
+    uint8_t magic[4];  // MKTK
+    uint64_t device_id;
+    uint32_t interface_name[IF_NAMESIZE];
+    uint64_t mac;  // 2 msb are 0
+    uint32_t ipv4;
+    uint8_t ipv6[16];
+};
+
+struct Device {};
+
+struct GlobalData {
+    uint64_t device_id;
+    std::unordered_map<uint64_t, Device> store;
+};
 
 int main() {
-    struct ifaddrs* ifaddr;
-    char host[NI_MAXHOST];
-
-    if (getifaddrs(&ifaddr) == -1) {
-        printf("error\n");
-        return -1;
-    }
-
-    while (ifaddr != NULL) {
-        if (ifaddr->ifa_addr == NULL)
-            continue;
-
-        int family = ifaddr->ifa_addr->sa_family;
-
-        ifaddr = ifaddr->ifa_next;
-    }
     return 0;
 }
+
+// int main() {
+//     struct ifaddrs* ifaddr;
+//     char host[NI_MAXHOST];
+//
+//     if (getifaddrs(&ifaddr) == -1) {
+//         printf("error\n");
+//         return -1;
+//     }
+//
+//     while (ifaddr != NULL) {
+//         if (ifaddr->ifa_addr == NULL)
+//             continue;
+//
+//         int family = ifaddr->ifa_addr->sa_family;
+//
+//         ifaddr = ifaddr->ifa_next;
+//     }
+//     return 0;
+// }
