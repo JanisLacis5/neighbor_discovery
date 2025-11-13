d71f319aa63ed8180283f69e7db2bbaa83a80b67
Janis Edvarts Lacis <janislacis06@gmail.com>
2025-11-13 14:09:22 +0200

    skech out the plan for cli

--- src/cli.cpp
+++ src/cli.cpp
@@ -1,3 +1,12 @@
-int main() {
+int main(int argc, char** argv) {
+    // TODO: open a new socket
+
+    // TODO: process users input
+
+    // TODO: create buffer to send to the daemon
+
+    // TODO: send the buffer and process the response
+
+    // TODO: print pretty result to the stdout
     return 0;
 }
